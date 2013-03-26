7d9df36aa830aa3adda8acacdbfdcc8419bb9835
Andrew Gerst <gerst20051@gmail.com>
2013-03-25 22:18:20 -0400

    fixed details

--- src/quotes/mysql.config.php
+++ src/quotes/mysql.config.php
@@ -1,7 +1,7 @@
 <?php
 define('MYSQL_HOST','localhost');
-define('MYSQL_USER','root');
-define('MYSQL_PASSWORD','comwiz05');
+define('MYSQL_USER','');
+define('MYSQL_PASSWORD','');
 define('MYSQL_DATABASE','hnswave');
 define('MYSQL_TABLE','quotes');
 ?>
