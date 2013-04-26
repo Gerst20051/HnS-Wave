2a500298eb8d2840c026902158b791db4348aa16
Andrew Gerst <gerst20051@gmail.com>
2013-04-26 18:31:08 -0400

    updated to git files

---
+++ src/quotes/mysql.config.backup.php
@@ -0,0 +1,8 @@
+<?php
+define('MYSQL_HOST','localhost');
+define('MYSQL_USER','');
+define('MYSQL_PASSWORD','');
+define('MYSQL_DATABASE','hnswave');
+define('MYSQL_TABLE','quotes');
+define('MYSQL_ALL_USERS','uid, email, username, firstname, lastname, access_level, last_login, date_joined, logins');
+?>
