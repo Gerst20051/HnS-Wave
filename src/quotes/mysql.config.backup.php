9ee8db4ca7e2849a2abe1e0d551a5a8e3ac202bf
Andrew Gerst <gerst20051@gmail.com>
2013-04-27 19:16:39 -0400

    updated to git files

--- src/quotes/mysql.config.backup.php
+++ src/quotes/mysql.config.backup.php
@@ -1,8 +1,18 @@
 <?php
-define('MYSQL_HOST','localhost');
-define('MYSQL_USER','');
-define('MYSQL_PASSWORD','');
-define('MYSQL_DATABASE','hnswave');
+require 'config.inc.php';
+
+if (LOCAL) {
+	define('MYSQL_HOST','');
+	define('MYSQL_USER','');
+	define('MYSQL_PASSWORD','');
+	define('MYSQL_DATABASE','');
+} else {
+	define('MYSQL_HOST','');
+	define('MYSQL_USER','');
+	define('MYSQL_PASSWORD','');
+	define('MYSQL_DATABASE','');
+}
+
 define('MYSQL_TABLE','quotes');
 define('MYSQL_ALL_USERS','uid, email, username, firstname, lastname, access_level, last_login, date_joined, logins');
 ?>
