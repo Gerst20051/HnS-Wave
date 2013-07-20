5e1a02f119534eef507864281d00f782565a83f7
Andrew Gerst <gerst20051@gmail.com>
2013-07-20 09:59:07 -0400

    finished create tables script

--- src/stations/json_to_db.php
+++ src/stations/json_to_db.php
@@ -3,7 +3,8 @@ header("Pragma: no-cache");
 header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 
-$updatedatabase = false;
+$createtables = true;
+$updatetables = false;
 $json = json_decode(file_get_contents('playlist.json'), true);
 $json = $json['data'];
 
@@ -11,39 +12,70 @@ $artists = array();
 
 foreach ($json as $key=>$v) {
 	$item = $json[$key];
-	$artist = $item['artist'];
-	unset($item['artist']);
-	$artists[$artist][] = $item;
+	$newitem = array();
+	$newitem['id'] = 0;
+	$newitem['videoid'] = $item['id'];
+	$newitem['title'] = $item['track'];
+	$newitem['img'] = $item['img'];
+	$newitem['duration'] = $item['duration'];
+	$artists[$item['artist']][] = $newitem;
 }
 
-/*
-foreach ($json as $key=>$v) {
-	$item = $json[$key];
-	foreach ($item as $attr=>$value) {
-		$row = $item;
-
-		//echo $value;
-		//echo "<br/>";
-		//echo $item[$track];
-	}
-	/*
-	echo $item['id'];
-	echo $item['artist'];
-	echo $item['track'];
-	echo $item['img'];
-	echo $item['duration'];
-	*
-}
-*/
-
-if ($updatedatabase === true) {
+if ($createtables === true) {
 	require_once 'mysql.class.php';
 
+	$db = new MySQL;
 	$artistnames = array_keys($artists);
+	$newartistnames = array();
 
+	foreach ($artistnames as $artistname) {
+		$names[] = array('id'=>0,'name'=>$artistname,'tracks'=>array());
+		$newartistnames[$artistname] = array('id'=>0,'tracks'=>array());
+	}
 
+	// insert artists
+	//// keep reference of artistid
+	foreach ($names as $name) {
+		$db->insert(MYSQL_TABLE_ARTISTS, array(
+			'name'=>$name['name'],
+			'tracks'=>json_encode($name['tracks'])
+		));
+		$newartistnames[$name['name']]['id'] = $db->insertID();
+	}
 
-	echo "database updated";
+	// insert tracks
+	foreach ($artists as $name => $tracks) {
+		foreach ($tracks as $key => $track) {
+			$db->insert(MYSQL_TABLE_TRACKS, array(
+				'artistid'=>$newartistnames[$name]['id'],
+				'videoid'=>$track['videoid'],
+				'title'=>$track['title'],
+				'img'=>$track['img'],
+				'duration'=>$track['duration']
+			));
+			$newartistnames[$name]['tracks'][] = $db->insertID();
+		}
+	}
+
+	// update artists with tracks. (array of tracks ids)
+	foreach ($names as $name) {
+		$tracksjson = json_encode($newartistnames[$name['name']]['tracks']);
+		$db->sfquery(array(
+			'UPDATE `%s`
+				SET tracks = "%s"
+					WHERE id = "%s"',
+			MYSQL_TABLE_ARTISTS,
+			$tracksjson,
+			$newartistnames[$name['name']]['id']
+		));
+	}
+
+	echo 'tables created';
+}
+
+if ($updatetables === true) {
+	// merge updates with old data
+	echo 'tables updated';
 }
 ?>
 
