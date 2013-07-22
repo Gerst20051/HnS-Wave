1ddd71cf87c82b92a56bf7c3c967d0621be7b222
Andrew Gerst <gerst20051@gmail.com>
2013-07-22 04:26:20 -0400

    fixed routes

--- src/stations/js/app.js
+++ src/stations/js/app.js
@@ -1,6 +1,7 @@
 window.App = Ember.Application.create({
+	LOG_ACTIVE_GENERATION: true,
 	LOG_TRANSITIONS: true,
-	currentPath: '',
+	LOG_VIEW_LOOKUPS: true,
 	isLocal: true,
 	namespace: function(){
 		return (App.get('isLocal')) ? 'git/hns-wave/src/stations/rest' : 'stations/rest';
@@ -9,49 +10,17 @@ window.App = Ember.Application.create({
 
 Ember.onerror = function(error){
 	console.log(error);
+	console.log(App.Router.router.recognizer.names);
 };
-
-/*
-App.ApplicationController = Ember.Controller.extend({
-	updateCurrentPath: function() {
-		App.set('currentPath', this.get('currentPath'));
-	}.observes('currentPath')
-});
-*/
 
 // Routes
 
 App.Router.map(function(){
 	this.resource('profile');
-	// URL: /profile
-	// RouteName: profile
-	// Controller: ProfileController
-	// Route: ProfileRoute
-	// Template: profile
 	this.resource('artists', function(){
-		// URL: /artists
-		// RouteName: artists OR artists.index
-		// Controller: ArtistsController OR ArtistsIndexController
-		// Route: ArtistsRoute OR ArtistsIndexRoute
-		// Template: artists OR artists/index
-		this.resource('artists.artist', { path: ':artist_id' }, function(){
-			// URL: /artists/:artist_id
-			// RouteName: artists.index OR artist.index
-			// Controller: ArtistsIndexController OR ArtistIndexController
-			// Route: ArtistsIndexRoute OR ArtistIndexRoute
-			// Template: artists/index OR artist/index
-			this.resource('artist.tracks', function(){
-				// URL: /artists/:artist_id/tracks
-				// RouteName: artists.tracks OR artists.artist.tracks OR artist.tracks
-				// Controller: ArtistsTracksController OR ArtistsArtistTracksController OR ArtistTracksController
-				// Route: ArtistsTracksRoute OR ArtistsArtistTracksRoute OR ArtistTracksRoute
-				// Template: artists/tracks OR artists/artist/tracks OR artist/tracks
+		this.resource('artist', { path: ':artist_id' }, function(){
+			this.resource('tracks', function(){
 				this.route('playing', { path: ':track_id' });
-					// URL: /artists/:artist_id/tracks/:track_id
-					// RouteName: tracks.index
-					// Controller: TracksIndexController
-					// Route: TracksIndexRouteRoute
-					// Template: tracks/index
 			});
 		});
 	});
@@ -63,14 +32,58 @@ App.IndexRoute = Ember.Route.extend({
 	}
 });
 
-App.ArtistsRoute = Ember.Route.extend({
+App.ProfileRoute = Ember.Route.extend({
 	model: function(params){
-		alert("Artists");
 		console.log(params);
-		return App.Artist.find();
 	}
 });
 
+App.ArtistsRoute = Ember.Route.extend({
+	model: function(params){
+		alert("ArtistsRoute");
+		console.log(params);
+		return App.Artist.find();
+	},
+	setupController: function(controller, tracks){
+		controller.set('content', tracks);
+	}
+});
+
+App.ArtistRoute = Ember.Route.extend({
+	model: function(params){
+		alert("ArtistRoute");
+		console.log(params);
+		return App.Artist.find(params.artist_id);
+	},
+	setupController: function(controller, tracks){
+		controller.set('content', tracks);
+	},
+	redirect: function(){
+		// this.transitionTo('tracks');
+	}
+});
+
+App.TracksRoute = Ember.Route.extend({
+	model: function(params){
+		alert("TracksRoute");
+		console.log(params);
+	},
+	setupController: function(controller, tracks){
+		controller.set('content', tracks);
+	}
+});
+
+App.PlayingRoute = Ember.Route.extend({
+	model: function(params){
+		alert("PlayingRoute");
+		console.log(params);
+	},
+	setupController: function(controller, tracks){
+		controller.set('content', tracks);
+	}
+});
+
+/*
 App.ArtistsIndexRoute = Ember.Route.extend({
 	model: function(params){
 		alert("ArtistsIndex");
@@ -141,60 +154,34 @@ App.TracksPlayingRoute = Ember.Route.extend({
 		controller.set('content', track);
 	}
 });
+*/
 
 // Controllers
 
-App.ArtistsController = Ember.ArrayController.extend({
-	addAndSearch: function(artist){
-		App.addArtist(artist);
-	}
+App.ProfileController = Ember.Controller.extend({
+
 });
 
-App.TrackController = Ember.ObjectController.extend({
+App.ArtistsController = Ember.ArrayController.extend({
+
+});
+
+App.ArtistController = Ember.Controller.extend({
+
+});
+
+App.TracksController = Ember.ObjectController.extend({
 	needs: "artist"
 });
 
-/*
-App.ArtistsTracksController = Ember.Object.create({
-	content: null,
-	populate: function(){
-		var controller = this;
-		$.getJSON(App.get('currentPath'), function(data){
-			controller.set('content', data);
-		});
-	}
-});
-*/
+App.PlayingController = Ember.Controller.extend({
 
-App.SearchController = Ember.Controller.extend({
-	needs: 'artists',
-	search: function(query){
-		this.get('controllers.artists').addAndSearch(query);
-	}
 });
 
 // Views
 
 App.TracksPlayingView = Ember.View.extend({
-	scBaseUrl: 'https://w.soundcloud.com/player/',
-	scTrackSourceUrlBinding: 'controller.model.url',
-	scIframeSourceUrl: function(){
-		return this.scBaseUrl + '?url=' + this.scTrackSourceUrl;
-	}.property()
-});
 
-App.SearchView = Ember.TextField.extend({
-	valueBinding: 'query',
-	didInsertElement: function(){
-		this.$().focus();
-	},
-	clearTextBox: function(){
-		this.$().val('');
-	},
-	change: function(e){
-		this.controller.search(this.query);
-		this.clearTextBox();
-	}
 });
 
 App.Store = DS.Store.extend({
@@ -203,11 +190,7 @@ App.Store = DS.Store.extend({
 		namespace: App.get('namespace')()
 	})
 });
-/*
-DS.RESTAdapter.configure("plurals", {
-	tracks: "tracks"
-});
-*/
+
 App.Artist = DS.Model.extend({
 	name: DS.attr('string'),
 	tracks: DS.hasMany('App.Track')
@@ -222,18 +205,4 @@ App.Track = DS.Model.extend({
 	duration: DS.attr('number'),
 	tracks: DS.belongsTo('App.Artist')
 });
-
-App.addArtist = function(name){
-	var artist = App.Artist.createRecord({ id: 33333, name: name });
-	var track = App.Track.createRecord({
-		id: 300333,
-		artistid: 33333,
-		videoid: "vid",
-		title: 'Jungle Mix',
-		url: 'https://api.soundcloud.com/justin-martin-music/justin-martin-jungle-mix',
-		img: 'http://i.ytimg.com/vi/zWktTuB41Ww/default.jpg',
-		duration: 259
-	});
-	artist.get('tracks').pushObject(track);
-};
 
