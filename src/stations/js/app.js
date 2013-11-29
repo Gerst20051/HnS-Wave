8f9711fafd4dd629467111a6bb507912f2c4f0e8
Andrew Gerst <gerst20051@gmail.com>
2013-11-29 18:53:24 -0500

    updated google analytics tracking code

--- src/stations/js/app.js
+++ src/stations/js/app.js
@@ -110,4 +110,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-42786295-1', 'hnswave.co');
 ga('send', 'pageview');
-
--- src/websnapchat/functions.js
+++ src/websnapchat/functions.js
@@ -365,9 +365,10 @@ ScreenCast.prototype.stop = function(){
 	// this.element.src = "";
 };
 
-(function(){
-	window._gaq=[['_setAccount','UA-36970192-1'],['_trackPageview']];
-	var ga=document.createElement('script');ga.type='text/javascript';ga.async=true;
-	ga.src=('https:'==document.location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';
-	var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ga,s);
-})();
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-42786295-1', 'hnswave.co');
+ga('send', 'pageview');
