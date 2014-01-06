009fe7c6f2a06d7ea8dc3e4571dc8faf752ef779
Andrew Gerst <gerst20051@gmail.com>
2014-01-06 15:13:34 -0500

    updated addLinks function to work in firefox

--- src/mastery/mastery.js
+++ src/mastery/mastery.js
@@ -4,7 +4,7 @@ if (!String.prototype.trim) {
 	};
 }
 
-var addLink = (function(){
+(function addLink(){
 	window.links = [];
 	return function(text, callback){
 		document.write('<div class="link" onclick="window.links['+window.links.length+']();">'+text+'</div>');
