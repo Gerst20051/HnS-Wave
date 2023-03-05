56bf0cd5a4652a1ced6f2b6c6af510aacd0f3bc1
Andrew Gerst <andrew.gerst@checkr.com>
2023-03-05 01:16:18 -0800

    add updates to paint racer

--- src/games/paint-racer/single-player/script.js
+++ src/games/paint-racer/single-player/script.js
@@ -1,4 +1,4 @@
-
+// const gameBoardSize = 5000;
 const gameBoardSize = 500;
 
 const DIRECTION = {
@@ -9,8 +9,57 @@ const DIRECTION = {
   DOWN: 2,
 };
 
+// http://jsfromhell.com/math/is-point-in-poly
+// https://gist.github.com/Sigmus/4318354
+// https://gist.github.com/fallenartist/538244d124448365a6fd
+// https://stackoverflow.com/questions/22521982/check-if-point-is-inside-a-polygon
+// https://en.wikipedia.org/wiki/Point_in_polygon
+// https://www.inkfood.com/collision-detection-with-svg
+function isPointInPoly(poly, pt) {
+  for (var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
+    ((poly[i].y <= pt.y && pt.y < poly[j].y) || (poly[j].y <= pt.y && pt.y < poly[i].y))
+    && (pt.x < (poly[j].x - poly[i].x) * (pt.y - poly[i].y) / (poly[j].y - poly[i].y) + poly[i].x)
+    && (c = !c);
+  return c;
+}
+
+function chooseRandomDirection() {
+  return randomElement([ DIRECTION.LEFT, DIRECTION.UP, DIRECTION.RIGHT, DIRECTION.DOWN ]);
+}
+
 function isPointInsideBoundingBox(pointX, pointY, minX, maxX, minY, maxY) {
   return minX < pointX && pointX < maxX && minY < pointY && pointY < maxY;
+}
+
+function getBoundingBoxFromPoints(points) {
+  let minX, maxX, minY, maxY;
+  points.forEach(point => {
+    if (typeof minX === 'undefined' || point.x < minX) minX = point.x;
+    if (typeof maxX === 'undefined' || point.x > maxX) maxX = point.x;
+    if (typeof minY === 'undefined' || point.y < minY) minY = point.y;
+    if (typeof maxY === 'undefined' || point.y > maxY) maxY = point.y;
+  });
+  return [ minX, maxX, minY, maxY ];
+}
+
+function getCenterOfBoundingBox(boundingBox) {
+  return {
+    x: boundingBox[0] + (boundingBox[1] - boundingBox[0]) / 2,
+    y: boundingBox[2] + (boundingBox[3] - boundingBox[2]) / 2,
+  };
+}
+
+function addAngleToCenterPointToPoints(center, points) {
+  for (let i = 0; i < points.length; i++) {
+    points[i].angle = Math.acos((points[i].x - center.x) / lineDistance(center, points[i]));
+    if (points[i].y > center.y) {
+      points[i].angle = TWO_PI - points[i].angle;
+    }
+  }
+}
+
+function sortPointsByAngleFromCenter(points) {
+  points = points.sort((a, b) => a.angle - b.angle);
 }
 
 function GameObject() {
@@ -37,21 +86,22 @@ function GameObject() {
 function MovableGameObject() {
   this.speed = 0;
   this.dir = DIRECTION.NONE;
+  this.nextDir = DIRECTION.NONE;
 
   this.goLeft = function () {
-    this.dir = DIRECTION.LEFT;
+    this.nextDir = DIRECTION.LEFT;
   };
 
   this.goUp = function () {
-    this.dir = DIRECTION.UP;
+    this.nextDir = DIRECTION.UP;
   };
 
   this.goRight = function () {
-    this.dir = DIRECTION.RIGHT;
+    this.nextDir = DIRECTION.RIGHT;
   };
 
   this.goDown = function () {
-    this.dir = DIRECTION.DOWN;
+    this.nextDir = DIRECTION.DOWN;
   };
 }
 
@@ -59,35 +109,151 @@ function GameBackground() {
   this.draw = function () {
     background(colors.carolinablue);
   };
-};
+}
 
 function GameBoard() {
   this.draw = function () {
-    fill(colors.whitesmoke);
     const { x, y } = this.getViewportOrigin();
+    fill(colors.whitesmoke);
     rect(x, y, gameBoardSize, gameBoardSize);
   };
-};
+}
 
 function Player() {
   this.radius = 10;
-  this.speed = 4;
+  this.speed = 1;
   this.dir = DIRECTION.NONE;
-
-  // TODO: Have a way to determine (relay / display) direction while playing game.
-  // TODO: Create initial base / territory.
+  this.color = randomGoldenRatioColor();
+  this.territory = [];
+  this.territories = [];
+  this.territoryColor = [];
+  this.tail = [];
+  this.wasInsideTerritory = true;
 
   this.init = function (x, y) {
     this.setPosition(x, y);
+    this.dir = chooseRandomDirection();
+    this.createInitialTerritory();
+  };
+
+  this.createInitialTerritory = function () {
+    this.territory = [
+      { x: this.x - (3 * this.radius), y: this.y - (3 * this.radius) },
+      { x: this.x + (3 * this.radius), y: this.y - (3 * this.radius) },
+      { x: this.x + (3 * this.radius), y: this.y + (3 * this.radius) },
+      { x: this.x - (3 * this.radius), y: this.y + (3 * this.radius) },
+      // { x: this.x - (3 * this.radius), y: this.y - (3 * this.radius) },
+    ];
   };
 
   this.run = function () {
+    this.checkTail();
+    this.checkDirection();
     this.move();
   };
 
   this.draw = function () {
+    const { x, y } = this.getViewportOrigin();
+    fill(this.color);
+    if (this.territory.length) {
+      // closePath();
+      path(this.territory.map(point => { return { x: x + point.x, y: y + point.y }; }));
+      // noClosePath();
+    }
+    this.territory.forEach(point => {
+      fill(colors.black);
+      circle(x + point.x, y + point.y, 4);
+    });
+    // if (this.territories.length) {
+    //   this.territories.forEach(function (territory, i) {
+    //     fill(this.territoryColor[i]);
+    //     closePath();
+    //     path(territory.map(point => { return { x: x + point.x, y: y + point.y }; }));
+    //     noClosePath();
+    //   }.bind(this));
+    // }
     fill(colors.red);
+    if (this.tail.length) {
+      // closePath();
+      path(this.tail.map(point => { return { x: x + point.x, y: y + point.y }; }));
+      // noClosePath();
+    }
+    stroke();
     circle(maxWidth / 2, maxHeight / 2, this.radius);
+    noStroke();
+  };
+
+  this.hasChangedDirection = function () {
+    return !(this.nextDir === DIRECTION.NONE || this.nextDir === this.dir);
+  };
+
+  this.checkDirection = function () {
+    if (!this.hasChangedDirection()) return;
+    this.dir = this.nextDir;
+    this.nextDir = DIRECTION.NONE;
+  };
+
+  this.checkTail = function () {
+    if (this.tail.length) {
+      if (this.isInsideTerritory()) {
+        this.tail.push({ x: this.x, y: this.y }, this.tail[0]);
+        // this.territoryColor.push(randomGoldenRatioColor());
+        // this.territories.push(this.tail);
+        // this.territory = simplify(martinez.union(
+        //   this.territory.map(point => [ point.x, point.y ]),
+        //   this.tail.map(point => [ point.x, point.y ])
+        // ).map(point => { return { x: point[0], y: point[1] }; }));
+        console.log('territory.length', this.territory.length);
+        console.log('tail.length', this.tail.length);
+        var alt = simplify(PolygonTools.polygon.union(
+          this.territory.map(point => [ point.x, point.y ]),
+          this.tail.map(point => [ point.x, point.y ])
+        )[0].map(point => { return { x: point[0], y: point[1] }; }));
+        console.log('alt1.length', alt.length);
+        if (alt.length < (this.territory.length / 2)) {
+          alt = simplify(PolygonTools.polygon.union(
+            this.tail.map(point => [ point.x, point.y ]),
+            this.territory.map(point => [ point.x, point.y ]),
+          )[0].map(point => { return { x: point[0], y: point[1] }; }));
+          console.log('alt2.length', alt.length);
+        }
+        this.territory = alt;
+        console.log('BEFORE this.territory', this.territory);
+        // const boundingBox = getBoundingBoxFromPoints(this.territory);
+        // const center = getCenterOfBoundingBox(boundingBox);
+        // addAngleToCenterPointToPoints(center, this.territory);
+        // sortPointsByAngleFromCenter(this.territory);
+        // console.log('AFTER this.territory', this.territory);
+        this.tail = [];
+        return;
+      }
+      const lastPoint = last(this.tail);
+      if (lastPoint.x === this.x && lastPoint.y === this.y) return;
+    }
+    if (!this.isInsideTerritory() && (this.wasInsideTerritory || this.hasChangedDirection())) {
+      const previousCoordinate = this.getPreviousCoordinate();
+      this.tail.push({
+        x: this.wasInsideTerritory ? previousCoordinate.x : this.x,
+        y: this.wasInsideTerritory ? previousCoordinate.y : this.y,
+      });
+      this.wasInsideTerritory = false;
+    }
+  };
+
+  this.isInsideTerritory = function () {
+    const isInsideTerritory = !!isPointInPoly(this.territory, { x: this.x, y: this.y });
+    if (isInsideTerritory) this.wasInsideTerritory = true;
+    return isInsideTerritory;
+  };
+
+  this.getPreviousCoordinate = function () {
+    if (this.dir === DIRECTION.NONE) return;
+    const prevX = this.x - (this.speed * this.dir);
+    const prevY = this.y - (this.speed * (this.dir < 0 ? -1 : 1));
+    return {
+      x: abs(this.dir) === 1 ? prevX : this.x,
+      y: abs(this.dir) === 1 ? this.y : prevY,
+    };
   };
 
   this.move = function () {
@@ -100,24 +266,40 @@ function Player() {
       if (isPointInsideBoundingBox(this.x, newY, 0, gameBoardSize, 0, gameBoardSize)) this.y = newY;
     }
   };
-};
+}
 
 function Enemy() {
   this.radius = 10;
   this.speed = 4;
   this.dir = DIRECTION.NONE;
   this.color = randomGoldenRatioColor();
+  this.territory = [];
 
   this.init = function (x, y) {
     this.setPosition(x, y);
+    this.createInitialTerritory();
+  };
+
+  this.createInitialTerritory = function () {
+    this.territory.push([
+      this.x - (3 * this.radius), // minX
+      this.x + (3 * this.radius), // maxX
+      this.y - (3 * this.radius), // minY
+      this.y + (3 * this.radius), // maxY
+    ]);
   };
 
   this.draw = function () {
-    fill(this.color);
     const { x, y } = this.getViewportOrigin();
+    fill(this.color);
+    this.territory.forEach(area => {
+      rect(x + area[0], y + area[2], area[1] - area[0], area[3] - area[2]); // minX, maxX, minY, maxY
+    });
+    stroke();
     circle(x + this.x, y + this.y, this.radius);
+    noStroke();
   };
-};
+}
 
 function Scene() {
   this.gameObjects = [];
@@ -127,10 +309,10 @@ function Scene() {
     this.configurePrototypes();
     this.gameObjects.push(new GameBackground());
     this.gameObjects.push(new GameBoard());
-    this.generateEnemy();
-    this.generateEnemy();
-    this.generateEnemy();
-    this.generateEnemy();
+    // this.generateEnemy();
+    // this.generateEnemy();
+    // this.generateEnemy();
+    // this.generateEnemy();
     this.generatePlayer();
   };
 
@@ -161,7 +343,7 @@ function Scene() {
       obj.draw && obj.draw();
     });
   };
-};
+}
 
 var setup = () => {
   fullScreen();
